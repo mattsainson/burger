bf64c8954e2f9c6c4da40ac1dc61b6bc469c393d
Matt Sainson <mattsainson@gmail.com>
2019-04-17 22:03:35 -0700

    Almost functional
    Most of the structure is in place.

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE `burgers` (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    burger_name VARCHAR(255),
+    devoured BOOLEAN
+);
---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chili", true);
+
+SELECT * FROM burgers;
